36e3d836cf67fb0fe5d5475874aa2bb548b54848
Sebastian_Pages <yves-sebastian.pages@etu.u-bordeaux.fr>
2019-11-18 17:29:45 +0100

    + unit class

--- src/game/Unit.java
+++ src/game/Unit.java
@@ -8,7 +8,6 @@ public class Unit extends Sprite{
 	
 	public Unit(Pane layer, Image image, double x, double y, int health,double damage, double speed) {
 		super(layer, image, x, y, health, damage);
-
 		setDy(speed);
 
 	}
