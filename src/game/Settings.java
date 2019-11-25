8d2410963c0bccd62adc41d42e0fd827bd54cdc1
Sebastian_Pages <yves-sebastian.pages@etu.u-bordeaux.fr>
2019-11-25 16:59:04 +0100

    added constants to settings file

--- src/game/Settings.java
+++ src/game/Settings.java
@@ -5,8 +5,19 @@ public class Settings {
 	public static final double SCENE_WIDTH = 800;
     public static final double SCENE_HEIGHT = 750;
 	public static final double STATUS_BAR_HEIGHT = 50;
-
-
+	
+	public static final int NUMBER_OF_CASTLES = 50;
+	public static final double NEUTRAL_PRODUCTION_SPEED = 0.1;
+	public static final double NORMAL_PRODUCTION_SPEED = 0.2;
+	public static final int NEUTRAL_MAX_UNIT = 20;
+	public static final int NORMAL_MAX_UNIT = 50;
+	
+	public static final int PIQUIER_COST= 100;
+	public static final int PIQUIER_PRODUCTION_TIME= 100;
+	public static final int PIQUIER_SPEED= 1;
+	public static final int PIQUIER_HEALTH= 1;
+	public static final int PIQUIER_DAMAGE= 1;
+	
     public static final double PLAYER_SPEED = 4.0;
     public static final int    PLAYER_HEALTH = 3;
     public static final double PLAYER_DAMAGE = 1;
@@ -20,5 +31,5 @@ public class Settings {
     public static final int FIRE_FREQUENCY_LOW = 1000 * 1000 * 1000; // 1 second in nanoseconds
     public static final int FIRE_FREQUENCY_MEDIUM = 500 * 1000 * 1000; // 0.5 second in nanoseconds
     public static final int FIRE_FREQUENCY_HIGH = 100 * 1000 * 1000; // 0.1 second in nanoseconds
-
+   
 }
